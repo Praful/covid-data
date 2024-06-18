ff06b6cb524b693d332084957dfb434c7b562244
praful <220836+Praful@users.noreply.github.com>
2024-06-19 00:48:25 +0100

    query country election results and show SWING when winning party changed from previous election

--- src/covid-data-analysis.py
+++ src/covid-data-analysis.py
@@ -8,6 +8,12 @@ import sqlite3
 conn = sqlite3.connect('../data/covid_database.db')
 
 
+def run(query):
+    result = pd.read_sql_query(query, conn)
+    # Display the query result
+    print(result)
+
+
 query = '''
     select
         cdc.state,
@@ -16,11 +22,11 @@ query = '''
         round(sum("covid-19 deaths")/wm.population*1000000) as "Deaths per 1m",
         rb.party
     from
-        cdc_covid cdc
+        cdc_covid_state cdc
     left join
         worldmeter_covid wm on cdc.state=wm."usa state"
     left join
-        red_blue rb on rb.state=cdc.state
+        red_blue_states rb on rb.state=cdc.state
     where
         cdc.`group`="By Month" and
         cdc."End Date" >= "2021-02-01"
@@ -29,9 +35,79 @@ query = '''
     order by
         "Deaths per 1m" desc;
 '''
-print(pd.read_sql_query(query, conn))
+#  print(pd.read_sql_query(query, conn))
+run(query)
 
-#  result = pd.read_sql_query(query, conn)
 
-# Display the query result
-#  print(result)
+county_election_winners = '''
+    WITH RankedVotes AS (
+        SELECT 
+            county_name,
+            county_fips,	
+            year,
+            party, 
+            candidatevotes,
+            ROW_NUMBER() OVER (PARTITION BY county_fips, year ORDER BY candidatevotes DESC) as rank
+        FROM 
+            harvard_county_pres_elections
+    )
+    SELECT 
+        county_name,
+        county_fips,
+        year,
+        party, 
+        candidatevotes
+    FROM 
+        RankedVotes
+    WHERE 
+        rank = 1
+    order by
+        county_fips
+'''
+
+
+county_election_winners_swing = '''
+    WITH RankedVotes AS (
+        SELECT 
+            county_name, 
+            county_fips, 
+            year,
+            party, 
+            candidatevotes,
+            ROW_NUMBER() OVER (PARTITION BY county_fips, year ORDER BY candidatevotes DESC) as rank
+        FROM 
+            harvard_county_pres_elections
+    ),
+    TopVotes AS (
+        SELECT 
+            county_name, 
+            county_fips, 
+            year,
+            party, 
+            candidatevotes,
+            ROW_NUMBER() OVER (PARTITION BY county_fips ORDER BY year) as year_rank,
+            LAG(party) OVER (PARTITION BY county_fips ORDER BY year) as previous_party
+        FROM 
+            RankedVotes
+        WHERE 
+            rank = 1
+    )
+    SELECT 
+        county_name, 
+        county_fips, 
+        year,
+        party, 
+        candidatevotes,
+        CASE 
+            WHEN party != previous_party THEN 'SWING'
+            ELSE party
+        END as change
+    FROM 
+        TopVotes
+    ORDER BY 
+        county_fips, year;
+
+'''
+
+run(county_election_winners)
+run(county_election_winners_swing)
